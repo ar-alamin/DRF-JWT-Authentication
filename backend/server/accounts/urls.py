e2853b908f20896a5396856ea6a7e4e44dead506
ar-alamin <aralamin.pi@yahoo.com>
2025-07-17 08:07:48 +0600

    reset password implementation

--- backend/server/accounts/urls.py
+++ backend/server/accounts/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('verify-email/', views.VerifyUserEmail.as_view(), name='verify'),
     path('login/', views.LoginUserView.as_view(), name='login'),
     path('profile/', views.TestAuthenticationView.as_view(), name='granted'),
+    path('password-reset/', views.PasswordResetRequestView.as_view(), name='password-reset'),
+    path('password-reset-confirm/<uidb64>/<token>/', views.PasswordResetConfirm.as_view(), name='password-reset-confirm'),
+    path('set-new-password/', views.SetNewPassword.as_view(), name='set-new-password'),
+    path('logout/', views.LogoutUserView.as_view(), name='logout'),
 ]
